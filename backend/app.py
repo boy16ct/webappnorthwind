686013f964dc7f02028dc583a5a9cc3683cd2f7f
boy16ct <boy16ct@gmail.com>
2019-12-04 19:59:18 +0700

    reupload

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,31 @@
 from flask import Flask
+import os
+import BusinessObjects as bo 
+import DataObjects as do
 
 app = Flask(__name__)
 
+db_ip = os.getenv("db_ip") #'10.0.2.15'
+ConnectionData = {}
+ConnectionData['user'] = 'postgres'
+ConnectionData['password'] = 'postgres'
+ConnectionData['host'] = str(db_ip)
+ConnectionData['port'] = '5432'
+ConnectionData['database'] = 'northwind'
+
 @app.route("/")
 def hello():
-    return 'hello world !!!'
+    c1 = bo.Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
+    return c1.CustomerName
+
+@app.route("/test_insert")
+def test_insert():
+    #ConnectionString = 'database=northwind user=postgres password=postgres host=10.0.2.15 port=5432'
+    c2 = do.Customer(ConnectionData)
+    c1 = bo.Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
+    s1 = c2.insert(c1)
+    return s1
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
